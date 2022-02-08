6439c1176d76285a06ca0a303ad3a127281668c4
Kayzu02 <98070725+Kayzu02@users.noreply.github.com>
2022-02-09 03:54:27 +0700

    Update config.py

--- SeiraRobot/config.py
+++ SeiraRobot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/SeiraRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/KayRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 2048978097  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "xyzsethhh"
-    SUPPORT_CHAT = "seirasupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "Kayzuuuuu"
+    SUPPORT_CHAT = "KayzuSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001180007354
     )  # Prints any new group the bot is added to, prints just the name and ID.
