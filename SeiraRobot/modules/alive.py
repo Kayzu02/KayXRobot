aaf89a303b7e87598982f3c569e14a1b6ea74375
Kayzu02 <98070725+Kayzu02@users.noreply.github.com>
2022-02-09 04:36:59 +0700

    Update alive.py

--- SeiraRobot/modules/alive.py
+++ SeiraRobot/modules/alive.py
@@ -12,12 +12,12 @@ PHOTO = "https://telegra.ph/file/51c4e8b6b40cd0cb66cea.jpg"
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm Seira!** \n\n"
-  LUNA += "💎 **I'm Working Properly** \n\n"
-  LUNA += "💎 **My Master : [ꜱᴇᴛʜ★](https://t.me/xyzsethhh)** \n\n"
-  LUNA += f"💎 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"💎 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Terima kasih sudah menambahkan Seira 💜**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/SeiraXRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/seirasupport")]]
+  LUNA = "**Holla I'm Kay!** \n\n"
+  LUNA += "⚡ **I'm Working Properly** \n\n"
+  LUNA += "⚡ **My Master : [ҡᴀʏ-ᴇx](https://t.me/Kayzuuuuu)** \n\n"
+  LUNA += f"⚡ **Telethon Version : {tlhver}** \n\n"
+  LUNA += f"⚡ **Pyrogram Version : {pyrover}** \n\n"
+  LUNA += "**Terima kasih sudah menambahkan Kay 💜**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/KayRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/KayzuSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
 
