d9be9f83d53a93c051ed39d4606dce42ae84d4c2
Kayzu02 <98070725+Kayzu02@users.noreply.github.com>
2022-03-04 06:56:46 +0700

    Update tagall.py

--- SeiraRobot/modules/tagall.py
+++ SeiraRobot/modules/tagall.py
@@ -9,7 +9,7 @@ from SeiraRobot.events import register as tomori
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "Hi Kawan Aku Seira Nimbrung you"
+    mentions = "Hai Aku Kay Nimbrung Yuk"
     chat = await event.get_input_chat()
     async for x in telethn.iter_participants(chat, 100):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
